4b2ac004484a00c1c30e0049e3dc85e7cbb94b2a
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-27 21:03:17 -0400

    commit tree 5d40421c760249fdc665fa0eb45db9c2f7d97c1d

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,22 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def username
+    !user.nil? ? user.username : ""
+  end
+
+  def username=(uname)
+    self.user = User.find_or_create_by(username: uname)
+    self.save
+  end
+
+  def user_attributes_username
+    username
+  end
+
+  def user_attributes_username=(uname)
+    username=(uname)
+  end
+
+
 end
